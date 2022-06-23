28b456b7487ac2fd3038fd2f6d49c2088cd9d945
Aligary <aligaryjr@gmail.com>
2022-06-22 17:28:25 -0700

    fixed code

--- bank-of-codepath-ui/src/components/Navbar/Navbar.jsx
+++ bank-of-codepath-ui/src/components/Navbar/Navbar.jsx
@@ -3,14 +3,26 @@ import FilterInput from "../FilterInput/FilterInput"
 import codepath from "../../assets/codepath.svg"
 import avatar from "../../assets/avatar.png"
 import "./Navbar.css"
+import { Link } from "react-router-dom"
 
-export default function Navbar() {
+export default function Navbar(props) {
+
+  function handleOnInputChange (e) {
+    props.setFilterInputValue(e.target.value)
+  }
+  
+  let path ="/"
   return (
     <nav className="navbar">
-      <a className="logo">Logo</a>
+      <a className="logo">
+        <Logo path={path}/>
+        </a>
 
       <div className="search">
-        <FilterInput />
+        <FilterInput 
+          inputValue={props.filterInputValue}
+          handleOnChange={handleOnInputChange}  
+        />
       </div>
 
       <div className="user">
@@ -30,10 +42,10 @@ export default function Navbar() {
   )
 }
 
-export function Logo() {
+export function Logo(props) {
   return (
     <a className="logo">
-      <img src={codepath} alt="logo" />
+      <Link to={props.path}><img src={codepath} alt="logo"/></Link>
     </a>
   )
 }
